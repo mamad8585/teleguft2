47dbb4db86cd86fe271a77337e3a4b79305dc23a
SoLiD <solid.in89@yahoo.com>
2016-02-28 09:05:07 +0330

    Update plugins.lua

--- plugins/plugins.lua
+++ plugins/plugins.lua
@@ -208,12 +208,12 @@ return {
           "!plugins reload : reloads all plugins." },
           },
   patterns = {
-    "^!plugins$",
-    "^!plugins? (+) ([%w_%.%-]+)$",
-    "^!plugins? (-) ([%w_%.%-]+)$",
-    "^!plugins? (+) ([%w_%.%-]+) (chat)",
-    "^!plugins? (-) ([%w_%.%-]+) (chat)",
-    "^!plugins? (reload)$" },
+    "^[/!]([Pp]lugins)$",
+    "^[/!]([Pp]lugins?) (+) ([%w_%.%-]+)$",
+    "^[/!]([Pp]lugins?) (-) ([%w_%.%-]+)$",
+    "^[/!]([Pp]lugins?) (+) ([%w_%.%-]+) (chat)",
+    "^[/!]([Pp]lugins?) (-) ([%w_%.%-]+) (chat)",
+    "^[/!]([Pp]lugins?) ([Rr]eload)$" },
   run = run,
   moderated = true, -- set to moderator mode
   --privileged = true
