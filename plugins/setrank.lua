077d608f0735780b15a3ce8b920cdac52911e779
SoLiD <solid.in89@yahoo.com>
2016-02-28 09:08:10 +0330

    Update setrank.lua

--- plugins/setrank.lua
+++ plugins/setrank.lua
@@ -226,6 +226,10 @@ return {
 	"^[/!]([Ii][Nn][Ff][Oo]) (.*)$",
 	"^[/!]([Ss][Ee][Tt][Rr][Aa][Nn][Kk]) (%d+) (.*)$",
 	"^[/!]([Ss][Ee][Tt][Rr][Aa][Nn][Kk]) (.*)$",
+        "^([Ii][Nn][Ff][Oo])$",
+	"^([Ii][Nn][Ff][Oo]) (.*)$",
+	"^([Ss][Ee][Tt][Rr][Aa][Nn][Kk]) (%d+) (.*)$",
+	"^([Ss][Ee][Tt][Rr][Aa][Nn][Kk]) (.*)$",
   },
   run = run
 }
